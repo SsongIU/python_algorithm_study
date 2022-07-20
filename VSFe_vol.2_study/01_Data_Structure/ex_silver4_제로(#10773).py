21a8fe8ea4c0fcf23843c1b1d3f5dab4a78bef4e
SsogIU <songin4229@naver.com>
2022-07-21 08:45:16 +0900

    072122 fix

--- VSFe_vol.2_study/01_Data_Structure/ex_silver4_제로(#10773).py
+++ VSFe_vol.2_study/01_Data_Structure/ex_silver4_제로(#10773).py
@@ -1,24 +1,29 @@
 from collections import deque
 
-data = deque()
-while True:
-    d = input("데이터 입력(나가기:q): ")
+
+def solution():
+    data = deque()
+    while True:
+        d = input("데이터 입력(나가기:q): ")
+
+        try:
+            data.append(int(d))
+        except ValueError:
+            data.append(0)
+
+        if d == "q" or d == "Q":
+            break
 
     try:
-        data.append(int(d))
-    except ValueError:
-        data.append(0)
+        data.remove(0)
+    except Exception:
+        pass
 
-    if d == "q" or d == "Q":
-        break
+    total = 0
+    while len(data):
+        total += data.pop()
 
-try:
-    data.remove(0)
-except Exception:
-    pass
+    return total
 
-total = 0
-while len(data):
-    total += data.pop()
 
-print(total)
+print(solution())
